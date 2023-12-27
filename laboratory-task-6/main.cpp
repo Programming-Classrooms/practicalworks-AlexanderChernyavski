978bf89775c17222c4dd57c0daa81c03c26d1c0f
AlexanderChernyavski <Rytrod@gmail.com>
2023-12-28 00:53:46 +0300

    Добавил изменения в рабораторную работу №6.Добавил дополнительную проверку

--- laboratory-task-6/main.cpp
+++ laboratory-task-6/main.cpp
@@ -1,12 +1,12 @@
-//Разработать шаблон функции для обработки массивов с элементами простого арифметического типа и массивов, элементы которых – строки типа char* .
-// Продемонстрировать использование этого шаблона для конкретных динамических массивов типа int, double, char* .
-//Для получения входных данных иметь три варианта :
-//непосредственный ввод и запись в текстовый файл;
-//генерирование с помощью датчика случайных чисел и запись в текстовый файл;
-//ввод из текстового файла, а вывод - в консоль.
-// Вариант 14.
-//Разработать шаблон функции, позволяющей сжать массив, удалив из него все элементы, равные заданному числу number.
-// Освободившиеся в конце массива элементы заполнить первым не удалённым.
+/*Разработать шаблон функции для обработки массивов с элементами простого арифметического типа и массивов, элементы которых – строки типа char* .
+Продемонстрировать использование этого шаблона для конкретных динамических массивов типа int, double, char* .
+Для получения входных данных иметь три варианта :
+непосредственный ввод и запись в текстовый файл;
+генерирование с помощью датчика случайных чисел и запись в текстовый файл;
+ввод из текстового файла, а вывод - в консоль.
+Вариант 14.
+Разработать шаблон функции, позволяющей сжать массив, удалив из него все элементы, равные заданному числу number.
+Освободившиеся в конце массива элементы заполнить первым не удалённым.*/
 
 
 #include <iostream>
@@ -17,11 +17,25 @@
 #include <string>
 
 
+size_t countByteOfFile()
+{
+    size_t counter = 0;
+    std::ifstream fin("file.txt");
+    while (!fin.eof())
+    {
+        fin.get();
+        ++counter;
+    }
+    fin.close();
+    counter--;
+    return counter;
+}
+
 template <typename T>
-void directInput(std::shared_ptr<T[]> array,  size_t size) 
+void directInput(std::shared_ptr<T[]> array, size_t size)
 {
     std::cout << "Enter " << size << " elements: \n";
-    for (size_t i = 0; i < size; ++i) 
+    for (size_t i = 0; i < size; ++i)
     {
         std::cin >> array[i];
     }
@@ -38,13 +52,14 @@ void arrayOutput(std::shared_ptr<T[]> array, size_t size)
 }
 
 template <typename T>
-void compressArray(std::shared_ptr<T[]> array, size_t size, T number) {
+void compressArray(std::shared_ptr<T[]> array, size_t size, T number)
+{
     T writeIndex = 0;
     size_t j = 0;
     size_t counter = 0;
-    for (size_t readIndex = 0; readIndex < size; ++readIndex) 
+    for (size_t readIndex = 0; readIndex < size; ++readIndex)
     {
-        if (fabs(number - array[readIndex]) > 0.0001) 
+        if (fabs(number - array[readIndex]) > 0.0001)
         {
             array[j++] = array[readIndex];
         }
@@ -54,13 +69,13 @@ void compressArray(std::shared_ptr<T[]> array, size_t size, T number) {
             ++counter;
         }
     }
-    for (size_t i = j; i < size; ++i) 
+    for (size_t i = j; i < size; ++i)
     {
         array[i] = writeIndex;
     }
 }
 
-//Рандом для инта (я определил от 0 до 100)
+// Рандом для инта (я определил от 0 до 100)
 void arrayRand(std::shared_ptr<int32_t[]> array, size_t size)
 {
     for (size_t i = 0; i < size; ++i)
@@ -72,7 +87,6 @@ void arrayRand(std::shared_ptr<int32_t[]> array, size_t size)
 // Рандом длявещественных значений
 void arrayRand(std::shared_ptr<double[]> array, size_t size)
 {
-
     for (size_t i = 0; i < size; ++i)
     {
         array[i] = 10 + (static_cast<double>(rand()) * 90 / RAND_MAX);
@@ -89,26 +103,31 @@ void arrayRand(std::shared_ptr<char[]> array, size_t size)
 }
 
 template <typename T>
-void writeToFile(std::shared_ptr<T[]> array, size_t size) {
+void writeToFile(std::shared_ptr<T[]> array, size_t size)
+{
     std::ofstream fout("file.txt");
-    if (fout.is_open()) 
+    if (fout.is_open())
     {
-        for (size_t i = 0; i < size; ++i) 
+        for (size_t i = 0; i < size; ++i)
         {
             fout << array[i] << " ";
         }
         fout.close();
-            std::cout << "Data written to file.txt\n";
+        std::cout << "Data written to file.txt\n";
     }
-    else 
+    else
     {
         throw std::exception("Unable to open file!\n");
     }
 }
 
 template <typename T>
-void writeOutOfFile(std::shared_ptr<T[]>  array, uint32_t size)
+void writeOutOfFile(std::shared_ptr<T[]> array, uint32_t size)
 {
+    if (countByteOfFile() != size)
+    {
+        throw std::exception("The size of the array you entered does not match the number of elements in the file");
+    }
     std::ifstream fin("file.txt");
     uint32_t countSymbol = 0;
     fin.seekg(0, std::ios_base::end);
@@ -127,7 +146,7 @@ void writeOutOfFile(std::shared_ptr<T[]>  array, uint32_t size)
         }
         else
         {
-           throw std::exception("Unable to open file!");
+            throw std::exception("Unable to open file!");
         }
     }
     else
@@ -137,7 +156,8 @@ void writeOutOfFile(std::shared_ptr<T[]>  array, uint32_t size)
 }
 
 template <typename T>
-void selectInputOption(std::shared_ptr<T[]> array, size_t  size) {
+void selectInputOption(std::shared_ptr<T[]> array, size_t size)
+{
     uint32_t choiceForTask;
     T numberToDelete = 0;
     std::cout << "Choose an option:\n";
@@ -147,10 +167,10 @@ void selectInputOption(std::shared_ptr<T[]> array, size_t  size) {
     std::cout << "What did you choose: ";
     std::cin >> choiceForTask;
     std::cout << "\n";
-    switch (choiceForTask) 
+    switch (choiceForTask)
     {
     case 1:
-        directInput( array, size);
+        directInput(array, size);
         std::cout << "Generated array: ";
         arrayOutput(array, size);
         std::cout << "Write element to delete: ";
@@ -173,14 +193,14 @@ void selectInputOption(std::shared_ptr<T[]> array, size_t  size) {
         break;
     case 3:
         writeOutOfFile(array, size);
-        std::cout << "Generated array: "; 
-         arrayOutput(array, size);
-         std::cout << "Write element to delete: ";
-         std::cin >> numberToDelete;
-         compressArray(array, size, numberToDelete);
-         std::cout << "Converted array: ";
-         arrayOutput(array, size);
-         writeToFile(array, size);
+        std::cout << "Generated array: ";
+        arrayOutput(array, size);
+        std::cout << "Write element to delete: ";
+        std::cin >> numberToDelete;
+        compressArray(array, size, numberToDelete);
+        std::cout << "Converted array: ";
+        arrayOutput(array, size);
+        writeToFile(array, size);
         break;
     default:
         throw std::runtime_error("Invalid choice. Choose between 1, 2, and 3.\n");
@@ -195,25 +215,24 @@ void checkSize(int32_t size)
     }
 }
 
-int main() {
+int main()
+{
     srand(time(NULL));
     try
     {
-    int32_t size;
-    std::cout << "Enter the size of the array:\n";
-    std::cin >> size;
-    checkSize(size);
-    std::shared_ptr <int32_t[]> arrayInt(new int32_t[size]);
-    std::shared_ptr <double[]> arrayDouble(new double[size]);
-    std::shared_ptr <char[]> arrayChar(new char[size]);
-    uint32_t choiceForArray;
-    std::cout << "Choose which array you want:\n";
-    std::cout << "1. With integer\n";
-    std::cout << "2. With fractions\n";
-    std::cout << "3.With symbols\n";
-    std::cout << "What did you choose: ";
-    try
-    {
+        int32_t size;
+        std::cout << "Enter the size of the array:\n";
+        std::cin >> size;
+        checkSize(size);
+        std::shared_ptr<int32_t[]> arrayInt(new int32_t[size]);
+        std::shared_ptr<double[]> arrayDouble(new double[size]);
+        std::shared_ptr<char[]> arrayChar(new char[size]);
+        uint32_t choiceForArray;
+        std::cout << "Choose which array you want:\n";
+        std::cout << "1. With integer\n";
+        std::cout << "2. With fractions\n";
+        std::cout << "3.With symbols\n";
+        std::cout << "What did you choose: ";
         std::cin >> choiceForArray;
         std::cout << '\n';
         switch (choiceForArray)
@@ -231,15 +250,9 @@ int main() {
             throw std::runtime_error("Invalid choice. Choose between 1, 2, and 3.\n");
         }
     }
-    catch (std::exception& isa)
-    {
-        std::cerr << isa.what();
-    }
-}
-    catch (std::exception& is)
+    catch (std::exception &is)
     {
         std::cerr << is.what();
     }
     return 0;
 }
-
